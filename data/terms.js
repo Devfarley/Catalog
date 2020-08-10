42db86380c53597659053a6880bf17cda0964e42
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-08-10 14:37:06 -0600

    implimented read fuction in DAL

---
+++ data/terms.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const {ObjectId} = require('mongodb');
+
+const url = process.env.DB_URL
+const db_name = process.env.DB_NAME
+const col_name = process.env.COL_NAME
+const options = {
+    useUnifiedTopology: true
+}
+
+const readTerms = () => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url,options, (err,client) => {
+            assert.equal(err, null);
+
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+            collection.find({}).toArray((err,docs) => {
+                assert.equal(err, null);
+                resolve(docs);
+                client.close();
+            });
+        });
+    });
+    return iou
+}
+
+
+module.exports = {
+    readTerms,
+}
