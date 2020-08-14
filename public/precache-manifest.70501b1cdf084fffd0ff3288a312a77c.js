984bb3dc7c7da0d8d6a982424b07e4f335dcd25b
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-08-14 10:13:18 -0600

    moved build into public

---
+++ public/precache-manifest.70501b1cdf084fffd0ff3288a312a77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96be2deb80567694d51a7cca1acd4070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9723589a57eb90167646",
+    "url": "/static/css/main.651c3513.chunk.css"
+  },
+  {
+    "revision": "252eb14f1f66ad9b07a3",
+    "url": "/static/js/2.8e9d8d49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e9d8d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9723589a57eb90167646",
+    "url": "/static/js/main.9938dc7a.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
